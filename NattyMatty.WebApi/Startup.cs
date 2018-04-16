70e44562cf787d93fab7a914744591cba2f212c6
dejvimax <davie_max@hotmail.com>
2018-04-16 19:54:21 +0100

    Removed including XML comments for Swagger due to issues with Azure deployment

--- NattyMatty.WebApi/Startup.cs
+++ NattyMatty.WebApi/Startup.cs
@@ -38,10 +38,11 @@ namespace NattyMatty.WebApi
                     }
                 });
 
+                //TODO: this section of code breaks Azure deployment
                 // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                //var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
             });            
         }
 
