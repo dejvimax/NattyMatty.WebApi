c428a6b341ab5a22bbc2b0213fe425cad0053906
dejvimax <davie_max@hotmail.com>
2018-09-10 19:05:49 +0100

    Remove commented code

--- NattyMatty.WebApi/ClientApp/src/app/components/product/product.component.ts
+++ NattyMatty.WebApi/ClientApp/src/app/components/product/product.component.ts
@@ -32,17 +32,7 @@ export class ProductComponent {
                 .subscribe(product => {this.product = product;},
                 error => console.error(error));
             
-            console.log("After Calling Product Service");
-
-            /* 
-            var url = this.baseUrl + "api/product/" + id;
-            //var url = "http://localhost:54895/product/" + id;
-
-            this.http.get<Product>(url).subscribe(result => {
-                this.product = result;
-            }, error => console.error(error));
-
-            */
+            console.log("After Calling Product Service");            
         }
         else {
             console.log("Invalid id: routing back to product...");
