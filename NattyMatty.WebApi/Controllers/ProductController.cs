7448140ed4517d38f431230eca299d7a214892e7
David Maxfield <David.Maxfield@ssp-worldwide.com>
2018-11-02 08:45:45 +0000

    Updated ProductController CRUD methods

--- NattyMatty.WebApi/Controllers/ProductController.cs
+++ NattyMatty.WebApi/Controllers/ProductController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using System;
+using Mapster;
+using NattyMatty.WebApi.ViewModels;
 
 namespace NattyMatty.WebApi.Controllers
 {
@@ -41,7 +43,14 @@ namespace NattyMatty.WebApi.Controllers
 
             var result = _context.Products.ToList();
 
-            return new JsonResult(result
+            // return new JsonResult(result
+            //     , new JsonSerializerSettings()
+            //     {
+            //         Formatting = Formatting.Indented
+            //     });
+
+
+            return new JsonResult(result.Adapt<List<ProductViewModel>>()
                 , new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
@@ -64,12 +73,114 @@ namespace NattyMatty.WebApi.Controllers
 
             _logger.LogInformation(LoggingEvents.GetProduct, $"Product '{product.Name}' found for Id: '{id}'");
             
-            return new JsonResult(product
+            return new JsonResult(product.Adapt<ProductViewModel>()
                 , new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
                 });
         }
+
+        /// <summary>
+        /// Adds a new Product to the Database
+        /// </summary>
+        /// <param name="model">The ProductViewModel containing the data to insert</param>
+        [HttpPut]
+        public IActionResult Put([FromBody]ProductViewModel model)
+        {
+            // return a generic HTTP Status 500 (Server Error)
+            // if the client payload is invalid.
+            if (model == null) return new StatusCodeResult(500);
+
+            // map the ViewModel to the Model
+            var product = model.Adapt<Product>();            
+
+            _context.Products.Add(product);
+            _context.SaveChanges();            
+
+            // return the newly-created Product to the client.
+            return new JsonResult(product.Adapt<ProductViewModel>()
+                , new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// Edit the Product with the given {id}
+        /// </summary>
+        /// <param name="model">The ProductViewModel containing the data to update</param>
+        [HttpPost]
+        public IActionResult Post([FromBody]ProductViewModel model)
+        {
+            // return a generic HTTP Status 500 (Server Error)
+            // if the client payload is invalid.
+            if (model == null) return new StatusCodeResult(500);
+
+            // retrieve the product to edit
+            var product = _context.Products.Where(p => p.Id ==
+                        model.Id).FirstOrDefault();
+
+            // handle requests asking for non-existing quizzes
+            if (product == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Product ID {0} has not been found", model.Id)
+                });
+            }
+
+            // handle the update (without object-mapping)
+            //   by manually assigning the properties 
+            //   we want to accept from the request
+            product.Name = model.Name;            
+
+            // persist the changes into the Database.
+            _context.SaveChanges();
+
+            // return the updated Quiz to the client.
+            return new JsonResult(product.Adapt<ProductViewModel>()
+                , new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// Deletes the Product with the given {id} from the Database
+        /// </summary>
+        /// <param name="id">The ID of an existing Product</param>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // retrieve the quiz from the Database
+            var product = _context.Products.Where(i => i.Id == id)
+                .FirstOrDefault();
+
+            // handle requests asking for non-existing products
+            if (product == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Product ID {0} has not been found", id)
+                });
+            }
+
+            // remove the product from the DbContext.
+            _context.Products.Remove(product);
+            // persist the changes into the Database.
+            _context.SaveChanges();
+
+            // return an HTTP Status 200 (OK).
+            // return new OkResult();
+
+            // [2018.01.26] BOOK ERRATA: return a NoContentResult to comply with a bug in the Angular 5 HttpRouter (fixed on December 2017, see here: https://github.com/angular/angular/issues/19502) which expects a JSON by default 
+            // and will throw a SyntaxError: Unexpected end of JSON input in case of an HTTP 200 result with no content.
+            // ref.: https://github.com/angular/angular/issues/19502
+            // ref.: https://github.com/PacktPublishing/ASP.NET-Core-2-and-Angular-5/issues/19
+            return new NoContentResult();
+        }
+
+        /*
 
         /// <summary>
         /// Creates a Product.
@@ -141,7 +252,7 @@ namespace NattyMatty.WebApi.Controllers
             _context.Products.Remove(todo);
             _context.SaveChanges();
             return new NoContentResult();
-        }
+        }*/ 
     }
 }
 
